7b340373365583a004d32ab8ab404cff2f120fd1
NickEU <neverate@gmail.com>
2020-10-29 06:39:23 +0200

    +DI container configurator

---
+++ DIContainerConfigurator/DIConfig.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using BusinessLayer;
+using DAL.Impl.Repos;
+using DAL.Interfaces;
+
+namespace DIContainerConfigurator
+{
+    public class DIConfig
+    {
+        public static void SetupContainer(string contextType)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MockUserRepoTest>().As<IUserRepo>();
+            if (contextType == "test")
+            {
+                builder.RegisterType<MockUserRepoTest>().As<IUserRepo>();
+            } else
+            {
+                builder.RegisterType<MockUserRepoProd>().As<IUserRepo>();
+            }
+            builder.RegisterType<UserService>().As<IUserService>();
+            var container = builder.Build();
+            InitBL.SetContainer(container);
+        }
+    }
+}
