814248056a55556c760d176db64aa9869a6f06dc
NickEU <neverate@gmail.com>
2020-10-29 06:40:25 +0200

    BusinessLayer: stub functionality

---
+++ BusinessLayer/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public interface IUserService
+    {
+        IReadOnlyCollection<string> GetNames();
+    }
+}
---
+++ BusinessLayer/UserService.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using DAL.Interfaces;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public class UserService : IUserService
+    {
+        public IReadOnlyCollection<string> GetNames()
+        {
+            var container = InitBL.Container;
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var repo = scope.Resolve<IUserRepo>();
+                return repo.GetNames();
+            }
+        }
+    }
+}
