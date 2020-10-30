7f1140a5f0116e4c1375f61449b5cb36a788b21e
NickEU <neverate@gmail.com>
2020-10-30 13:25:00 +0200

    BL: +IProductService +ProductService

---
+++ BusinessLayer/Implementations/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Implementations
+{
+    public class ProductService : IProductService
+    {
+        public IReadOnlyCollection<string> GetAllBrands()
+        {
+            var container = InitBL.Container;
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var repo = scope.Resolve<IProductRepo>();
+                return repo.GetAllBrands();
+            }
+        }
+    }
+}
