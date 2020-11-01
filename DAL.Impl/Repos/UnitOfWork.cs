8842335b5589c2773107dfc817359ab4a5bc7fdf
NickEU <neverate@gmail.com>
2020-11-01 21:30:29 +0200

    Upgraded from packages.config to PackageReference

---
+++ DAL.Impl/Repos/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+
+namespace DAL.Impl.Repos
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        public IProductRepo ProductRepo { get; }
+        public IUserRepo UserRepo { get; }
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
