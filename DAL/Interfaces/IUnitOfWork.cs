f069a51cb462be4e0365eb788d195e99eeaa3a2a
NickEU <neverate@gmail.com>
2020-11-01 15:31:06 +0200

    DAL: cleaned up the code

--- DAL/Interfaces/IUnitOfWork.cs
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -1,11 +1,4 @@
-﻿using DAL.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DAL.UOW
+﻿namespace DAL.Interfaces
 {
     public interface IUnitOfWork
     {
