e8cf6cc17bc706fd99215bd1db03748679ddab3d
NickEU <neverate@gmail.com>
2020-11-01 15:30:42 +0200

    BL: cleaned up the code

--- BusinessLayer/Implementations/ProductService.cs
+++ BusinessLayer/Implementations/ProductService.cs
@@ -1,10 +1,7 @@
 ﻿using Autofac;
 using DAL.Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BusinessLayer.Interfaces;
 
 namespace BusinessLayer.Implementations
 {
--- BusinessLayer/Interfaces/IProductService.cs
+++ BusinessLayer/Interfaces/IProductService.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace BusinessLayer
+namespace BusinessLayer.Interfaces
 {
     public interface IProductService
     {
