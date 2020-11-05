bdc3d20f43b88cab22e93fbca759ce5f0e416063
NickEU <neverate@gmail.com>
2020-11-05 20:04:23 +0200

    MVC: major refactoring. -added Brands/Items controllers -added ListItems action/view -stub for a ProductViewModel

---
+++ FancyOnlineStore/Controllers/BrandsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Interfaces;
+
+namespace FancyOnlineStore.Controllers
+{
+    public class BrandsController : Controller
+    {
+        private readonly IServices _services;
+
+        public BrandsController(IServices services)
+        {
+            _services = services;
+        }
+
+        public ActionResult ListBrands()
+        {
+            return View(_services.Brand.GetNames());
+        }
+    }
+}
---
+++ FancyOnlineStore/Controllers/ItemsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Interfaces;
+using DAL.Models;
+using FancyOnlineStore.Models;
+
+namespace FancyOnlineStore.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly IServices _services;
+
+        public ItemsController(IServices services)
+        {
+            _services = services;
+        }
+
+        public ActionResult ListItems()
+        {
+            var products = _services.Product
+                .GetProductsWithClothingType(ClothingType.Jacket)
+                //TODO: add auto mapping
+                .Select(p => new ProductViewModel
+                {
+                    BrandName = p.BrandName, Type = p.Type
+                })
+                .ToList();
+            return View(products);
+        }
+    }
+}
