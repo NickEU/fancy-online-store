58626d942bfc6545d81bf374a2efd40ec2be2ab2
NickEU <neverate@gmail.com>
2020-11-08 21:10:14 +0200

    MVC: removed "caching" that doesn't work ( doh, controllers are recreated on a new request)

--- FancyOnlineStore/Controllers/ItemsController.cs
+++ FancyOnlineStore/Controllers/ItemsController.cs
@@ -12,7 +12,6 @@ namespace FancyOnlineStore.Controllers
     public class ItemsController : Controller
     {
         private readonly IServices _services;
-        private List<ProductDto> _clothesCache;
 
         public ItemsController(IServices services)
         {
@@ -23,19 +22,18 @@ namespace FancyOnlineStore.Controllers
         public ActionResult ListItems(int? page)
         {
             ViewBag.Title = "Our collection";
-            _clothesCache = _clothesCache ?? _services.Product
+            // not the greatest solution in the world
+            var clothes = _services.Product
                 .GetProductsWithClothingType(ClothingType.Jacket)
                 .OrderByDescending(p => p.ProductName.Length)
                 .ThenByDescending(p => p.ProductName)
                 .ToList();
-
-            // might need to do some additional steps in the future
-            var clothes = _clothesCache;
             var clothesView = AutoMapper.Mapper
                 .Map<IEnumerable<ProductDto>, IEnumerable<ProductViewModel>>(clothes);
 
             const int pageSize = 10;
             var pageNum = page ?? 1;
+            // TODO: implement server-side pagination, using SQL stored procedure??
             return View(clothesView.ToPagedList(pageNum, pageSize));
         }
     }
