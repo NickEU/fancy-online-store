789343e73191a8c1e786e825600ff3246ec964a2
NickEU <neverate@gmail.com>
2020-11-08 12:05:42 +0200

    BL: tweaked return types of services to add consistency

--- BusinessLayer/Implementations/ProductService.cs
+++ BusinessLayer/Implementations/ProductService.cs
@@ -1,9 +1,7 @@
-﻿using Autofac;
+﻿using System.Collections.Generic;
 using DAL.Interfaces;
-using System.Collections.Generic;
 using System.Linq;
 using BusinessLayer.Interfaces;
-using System;
 using DAL.Models;
 
 namespace BusinessLayer.Implementations
@@ -19,7 +17,8 @@ namespace BusinessLayer.Implementations
         public IEnumerable<ProductDto> GetProductsWithClothingType(ClothingType clothingType)
         {
             return _repo.ProductRepo
-                .Find(p => p.Type == clothingType);
+                .Find(p => p.Type == clothingType)
+                .ToList();
         }
     }
 }
