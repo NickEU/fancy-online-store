cdacacffc0b934268716f807b837216ae1d1d82d
NickEU <neverate@gmail.com>
2020-11-09 09:23:45 +0200

    Added some TODOs to DAL models

--- DAL.Impl/Repos/EF/Models/ProductImage.cs
+++ DAL.Impl/Repos/EF/Models/ProductImage.cs
@@ -9,6 +9,8 @@ namespace DAL.Impl.Repos.EF.Models
         public Guid ProductImageId { get; set; }
         [ForeignKey("Product")]
         public Guid ProductId { get; set; }
+        // TODO: this sucks, better off storing SHA256 in DB 
+        // TODO: and storing images in a folder on server or on CDN
         public byte[] Image { get; set; }
 
         public virtual Product Product { get; set; }
